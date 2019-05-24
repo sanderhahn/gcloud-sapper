9e682871ceabc3015922f760e5208742f4e033f3
Sander Hahn <sanderhahn@gmail.com>
2019-05-24 02:47:33 +0200

    req.path workaround

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // gcloud-sapper/index.js
 const server = require('./__sapper__/build/server/server.js');
 exports.sapper = (req, res) => {
-    server.app.handler(req, res);
+  // define a path property setter on req
+  var path = req.path;
+  Object.defineProperty(req, 'path', {
+    get: function() {
+      return path;
+    },
+    set: function(newValue) {
+      path = newValue;
+    },
+  });
+  server.app.handler(req, res);
 };
